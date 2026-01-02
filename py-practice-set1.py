de8dbbbbe9d48a75d62a5445578f67646cb6e9fb
Adarsh <adarsh07poojari@gmail.com>
2026-01-02 19:25:52 +0530

    2st Version with using Dictionary

--- py-practice-set1.py
+++ py-practice-set1.py
@@ -4,14 +4,14 @@ A cashier has currency notes of denominations 1, 5 and 10. If the amount to be w
 amount=int(input("Enter the amount : "))
 if type(amount)!=int:
     amount=int(input("Enter the amount : "))
-amt=amount
-note_10= amount//10
+total_amt=amount
+note={10:0,5:0,1:0}
+note[10]=amount//10
 amount=amount%10
-note_5=amount//5
+note[5]=amount//5
 amount=amount%5
-note_1=amount
-
-print(f"Total Amount :{amt}\n 10 : {note_10}\n 5 : {note_5}\n 1 : {note_1}")
+note[1]=amount
+print(f"Total Amount :{total_amt}\n 10 : {note[10]}\n 5 : {note[5]}\n 1 : {note[1]}")
 
 
 
